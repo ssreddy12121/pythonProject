de8631a55454cb3e92feaa48de64a24e684850f0
Srinivas Reddy Sama <69422953+ssreddy12121@users.noreply.github.com>
2020-08-09 18:52:34 +0530

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,19 @@
+class sama:
+    """designing instance variable and method"""
+    girl="mouni"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def insta(self,marriage):
+        self.marriage=marriage
+        print("hii {} pilla ".format(sama.girl),"i know your age is:",self.age,"marriage",self.marriage)
+        print("i know your age:",self.age)
+s=sama('mounika',23)
+s.insta("inka kaledu")
+s.ready="yes iam ready for marriage"
+print(sama.girl)
+print(s.ready)
+print(s.name,s.age)
+del s.ready
+del s.marriage
+print(s.__dict__)
